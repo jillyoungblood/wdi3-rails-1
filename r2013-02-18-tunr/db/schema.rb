babe176450f4f70f6a4aee25edf8b8f57427d48c
Chyld Medford <chyld.medford@gmail.com>
2013-02-21 09:12:45 -0500

    tunr2

--- r2013-02-18-tunr/db/schema.rb
+++ r2013-02-18-tunr/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218162402) do
+ActiveRecord::Schema.define(:version => 20130220213559) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -68,8 +68,11 @@ ActiveRecord::Schema.define(:version => 20130218162402) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "image"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+    t.string   "password_digest"
+    t.boolean  "is_admin",        :default => false
+    t.decimal  "balance",         :default => 0.0
   end
 
 end
