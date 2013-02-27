c898b1a38301fb6abbcd85a124281ea17677a4a2
Chyld Medford <chyld.medford@gmail.com>
2013-02-27 09:20:15 -0500

    paintr

--- r2013-02-26-paintr/app/assets/javascripts/paintr.js
+++ r2013-02-26-paintr/app/assets/javascripts/paintr.js
@@ -1,10 +1,30 @@
 $(function(){
   $('#add_color').click(add_color);
-  $('#clear').click(clear);
+  $('#add_image').click(add_image);
+  $('#clear_colors').click(clear_colors);
   $('#colors').on('click', '.box', set_color);
   $('#canvas').on('mouseover', '.paint', paint_color);
+  $('#images').on('click', '.image_box', set_image);
   create_paint_boxes();
 });
+
+function set_image()
+{
+  var box = $(this);
+  var image = box.css('background-image');
+  $('#canvas').css('background-image', image);
+}
+
+function add_image()
+{
+  var image = $('<div>');
+  image.addClass('image_box');
+  var url = $('#image').val();
+  image.css('background-image', 'url('+url+')');
+  $('#images').prepend(image);
+  $('#image').val('');
+  $('#image').focus();
+}
 
 function paint_color()
 {
@@ -15,15 +35,15 @@ function paint_color()
 
 function create_paint_boxes()
 {
-  for(var i = 0; i < 5000; i++)
+  for(var i = 0; i < 2000; i++)
   {
     var paint = $('<div>');
     paint.addClass('paint');
-    $('#canvas').append(paint);
+    $('#canvas').prepend(paint);
   }
 }
 
-function clear()
+function clear_colors()
 {
   $('#colors').empty();
   $('#selected').css('background-color', 'white');
