9cc21d5fa384b9380941996319a19f808a5b1c4b
Chyld Medford <chyld.medford@gmail.com>
2013-02-28 17:40:18 -0500

    todo3

--- r2013-02-28-todo/app/assets/javascripts/todo.js
+++ r2013-02-28-todo/app/assets/javascripts/todo.js
@@ -1,8 +1,55 @@
 $(function(){
 
+  $('#new_priority').click(show_form);
+  $('#cancel_priority').click(hide_form);
+  $('#create_priority').click(create_priority);
+
   add_color_boxes();
+  init_minicolors();
 
 });
+
+function init_minicolors()
+{
+  var settings = {
+    animationSpeed: 100,
+    animationEasing: 'swing',
+    change: null,
+    changeDelay: 0,
+    control: 'hue',
+    defaultValue: '',
+    hide: null,
+    hideSpeed: 100,
+    inline: false,
+    letterCase: 'lowercase',
+    opacity: false,
+    position: 'default',
+    show: null,
+    showSpeed: 100,
+    swatchPosition: 'left',
+    textfield: false,
+    theme: 'default'
+};
+
+  $('INPUT.minicolors').minicolors(settings);
+}
+
+function create_priority()
+{
+  return false;
+}
+
+function hide_form()
+{
+  $('#new_priority').show();
+  $('.form').hide();
+}
+
+function show_form()
+{
+  $('#new_priority').hide();
+  $('.form').show();
+}
 
 function add_color_boxes()
 {
