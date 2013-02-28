191a64567ba5c7217e37645434fa340aa671793a
Chyld Medford <chyld.medford@gmail.com>
2013-02-28 16:20:38 -0500

    todo2

--- r2013-02-28-todo/app/assets/javascripts/todo.js
+++ r2013-02-28-todo/app/assets/javascripts/todo.js
@@ -1,3 +1,20 @@
 $(function(){
 
+  add_color_boxes();
+
 });
+
+function add_color_boxes()
+{
+  $('.color').each(change_text_to_color);
+}
+
+function change_text_to_color(index, element)
+{
+  var box = $(element);
+  var color = box.text();
+
+  box.removeClass('hide');
+  box.css('background-color', color);
+  box.text('');
+}
