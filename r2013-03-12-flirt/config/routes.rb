a2f76ff22730f684a575a66cf828dcc719adfc29
Chyld Medford <chyld.medford@gmail.com>
2013-03-13 16:23:07 -0400

    flirt c

--- r2013-03-12-flirt/config/routes.rb
+++ r2013-03-12-flirt/config/routes.rb
@@ -1,4 +1,6 @@
 R20130312Flirt::Application.routes.draw do
   root :to => 'home#index'
   resources :subscribers, :only => [:new, :create]
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
 end
